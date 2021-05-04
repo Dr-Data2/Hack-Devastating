40802fc586f0432fa49ccc716b24099153bbb640
alastorh2222 <83009351+alastorh2222@users.noreply.github.com>
2021-05-05 01:49:39 +0300

    Create tools-main.py

---
+++ tools-main.py
@@ -0,0 +1,167 @@
+import os
+import sys
+from time import sleep
+print(os.system('clear'))
+W = '\033[0m'  # white (normal)
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+O = '\033[33m'  # orange
+B = '\033[34m'  # blue
+P = '\033[35m'  # purple
+C = '\033[36m'  # cyan
+GR = '\033[37m'  # gray
+sleep(1)
+print( G +"""
+     .---. one for all  .------ALASTORH----------
+               /     \  __  /    ---DR.DATA---
+              / /     \(  )/    ------
+             //////   ' \/ `   ----
+            //// / // :    : ----
+           // /   /  /`    '---
+          //          //..\\
+        =============UU====UU====
+                     '//||\\`
+                       ''``
+""")
+sleep(.5)
+print('+                               +')
+sleep(.25)
+print(R+'+++++++++++++++++++++++++++++++++')
+sleep(.25)
+print('+                               +')
+sleep(.5)
+print('+             help              +')
+sleep(.5)
+print('+                               +')
+sleep(.25)
+print('+  1-python     +     2-git     +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  3-php        +     4-rube    +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  5-proot      +     6-tor     +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  7-nano       +     8-update  +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  9-upgrade    +     10-wget   +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  11-curl      +     12-zip    +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  13-unzip     +     14-unstab +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  15-hydra     +     16-meta   +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  17-html      +     18-css    +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+  19-java      +     20-setup  +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+           00-exit             +')
+sleep(.25)
+print('+                               +')
+sleep(.25)
+print('+++++++++++++++++++++++++++++++++')
+sleep(.5)
+print(G +'+                               +')
+aaaa = ('''
++                               +
++++++++++++++++++++++++++++++++++
++                               +                    
++  1-python     +     2-git     +
++                               +
++  3-php        +     4-rube    +
++                               +
++  5-proot      +     6-tor     +
++                               +
++  7-nano       +     8-update  +
++                               +
++  9-upgrade    +     10-wget   +
++                               +
++  11-curl      +     12-zip    +
++                               +
++  13-unzip     +     14-unstab +
++                               +
++  15-hydra     +     16-meta   +
++                               +
++  17-html      +     18-css    +
++                               +
++  19-java      +     20-setup  +
++                               +
++           00-exit             +
++                               +
++++++++++++++++++++++++++++++++++
++                               + 
+
++ Let your choice is successful 👍😜 + 
+
+''')
+
+
+
+
+while True:
+    a = input(' Enter your numper >>>> ')
+    if a == '1' :
+    print(os.system('pkg install python -y'))
+    elif a == '2' :
+    print(os.system('pkg install git -y'))
+    elif a == '3' :
+    print(os.system('pkg install php -y'))
+    elif a == '4' :
+    print(os.system('pkg install rube -y'))
+    elif a == '5' :
+    print(os.system('pkg install proot -y'))
+    elif a == '6' :
+    print(os.system('pkg install tor -y'))
+    elif a == '7' :
+    print(os.system('pkg install nano -y'))
+    elif a == '8' :
+    print(os.system('pkg update -y'))
+    elif a == '9' :
+    print(os.system('pkg upgrade -y'))
+    elif a == '10' :
+    print(os.system('pkg install wget -y'))
+    elif a == '11' :
+    print(os.system('pkg install curl -y'))
+    elif a == '12' :
+    print(os.system('pkg install zip -y'))
+    elif a == '13' :
+    print(os.system('pkg install unzip -y'))
+    elif a == '14' :
+    print(os.system('pkg install unstaple-repo -y'))
+    elif a == '15' :
+    print(os.system('pkg install hydra -y'))
+    elif a == '16' :
+    print(os.system('pkg install metasploit -y'))
+    elif a == '17' :
+    print(os.system('pkg install html -y'))
+    elif a == '18' :
+    print(os.system('pkg install css -y'))
+    elif a == '19' :
+    print(os.system('pkg install java'))
+    elif a == '20' :
+    print(os.system('termux-setup-storage'))
+    elif a == 'help':
+    print (aaaa)
+    elif a == '00':
+    sys.exit()
+    else:
+    print ('Error')
